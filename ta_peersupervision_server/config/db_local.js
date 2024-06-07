7f4cbe6637dc2a791bee133bd5e2a16d3eee4333
Michelle Angelina <160707395+hwamichelleangelina@users.noreply.github.com>
2024-06-07 10:41:18 +0700

    Create db_local.js

---
+++ ta_peersupervision_server/config/db_local.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+// Koneksi ke MySQL
+const mysqlConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'supervise',
+    database: 'peersupervision'
+})
+
+mysqlConn.connect(function (error) {
+    if (error) {
+        console.error('Error connecting to database:', err.message);
+    } else {
+        console.log('Connected to MySQL database: peersupervision.');
+    }
+});
+
+module.exports = mysqlConn;
